433b9a0480707ca8e81ceb1864eeb2df127ec4fa
Nicholas Cardoza <Nicholascardoza84@yahoo.com>
2023-01-19 20:05:52 +0000

    completed prep-javascript-for-loops"
    git status
    
    git commit -m "completed prep-javascript-for-loops"

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,11 @@
+for (var n = 0; n < 10; n++) {
+  console.log(n);
+}
+
+for (var n = 0; n < 20; n += 2) {
+  console.log(n);
+}
+
+for (var n = 100; n > 0; n--) {
+  console.log('Time till explosion:', n);
+}
